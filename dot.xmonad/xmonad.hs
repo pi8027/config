2704fa60b752ae2a20eb2769e5b6fa18ccd0d76b
kazuhiko Sakaguchi <pi8027@gmail.com>
2010-12-25 14:11:28 +0900

    .Xmodmap, xmonad.hs

--- dot.xmonad/xmonad.hs
+++ dot.xmonad/xmonad.hs
@@ -9,77 +9,28 @@ import qualified Data.Map        as M
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-
-    -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-
-    -- launch dmenu
-    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
-
-    -- launch gmrun
-    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
-
-    -- close focused window
-    , ((modm .|. shiftMask, xK_c     ), kill)
-
-     -- Rotate through the available layout algorithms
-    , ((modm,               xK_space ), sendMessage NextLayout)
-
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
-    -- Resize viewed windows to the correct size
-    , ((modm,               xK_n     ), refresh)
-
-    -- Move focus to the next window
-    , ((modm,               xK_Tab   ), windows W.focusDown)
-
-    -- Move focus to the next window
-    , ((modm,               xK_j     ), windows W.focusDown)
-
-    -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
-
-    -- Move focus to the master window
-    , ((modm,               xK_m     ), windows W.focusMaster  )
-
-    -- Swap the focused window and the master window
-    , ((modm,               xK_Return), windows W.swapMaster)
-
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-
-    -- Shrink the master area
-    , ((modm,               xK_h     ), sendMessage Shrink)
-
-    -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
-
-    -- Push window back into tiling
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-
-    -- Increment the number of windows in the master area
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-
-    -- Deincrement the number of windows in the master area
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-
-    -- Quit xmonad
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-
-    -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-    ]
+myKeys conf = M.fromList $ [
+    ((smodm, xK_Return), spawn $ XMonad.terminal conf),                           -- launch a terminal
+    ((modm,  xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\""), -- launch dmenu
+    ((smodm, xK_p     ), spawn "gmrun"),                                          -- launch gmrun
+    ((smodm, xK_c     ), kill),                                                   -- close focused window
+    ((modm,  xK_space ), sendMessage NextLayout),                                 -- Rotate through the available layout algorithms
+    ((smodm, xK_space ), setLayout $ XMonad.layoutHook conf),                     --  Reset the layouts on the current workspace to default
+    ((modm,  xK_n     ), refresh),                                                -- Resize viewed windows to the correct size
+    ((modm,  xK_Tab   ), windows W.focusDown),                                    -- Move focus to the next window
+    ((modm,  xK_j     ), windows W.focusDown),                                    -- Move focus to the next window
+    ((modm,  xK_k     ), windows W.focusUp),                                      -- Move focus to the previous window
+    ((modm,  xK_m     ), windows W.focusMaster),                                  -- Move focus to the master window
+    ((modm,  xK_Return), windows W.swapMaster),                                   -- Swap the focused window and the master window
+    ((smodm, xK_j     ), windows W.swapDown),                                     -- Swap the focused window with the next window
+    ((smodm, xK_k     ), windows W.swapUp),                                       -- Swap the focused window with the previous window
+    ((modm,  xK_h     ), sendMessage Shrink),                                     -- Shrink the master area
+    ((modm,  xK_l     ), sendMessage Expand),                                     -- Expand the master area
+    ((modm,  xK_t     ), withFocused $ windows . W.sink),                         -- Push window back into tiling
+    ((modm,  xK_comma ), sendMessage (IncMasterN 1)),                             -- Increment the number of windows in the master area
+    ((modm,  xK_period), sendMessage (IncMasterN (-1))),                          -- Deincrement the number of windows in the master area
+    ((smodm, xK_q     ), io $ exitWith ExitSuccess),                              -- Quit xmonad
+    ((modm,  xK_q     ), spawn "xmonad --recompile; xmonad --restart")]           -- Restart xmonad
     ++
 
     --
@@ -87,9 +38,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-shift-[1..9], Move client to workspace N
     --
     [((m .|. modm, k), windows s)
-        | (i, k) <- zip (XMonad.workspaces conf)
-            [xK_exclam, xK_at, xK_ssharp{-ちがう-}, xK_dollar, xK_percent,
-            xK_asciicircum, xK_ampersand, xK_asterisk, xK_parenleft, xK_parenright]
+        | (i, k) <- zip (workspaces conf) numsyms
         , (s, m) <- [(W.greedyView i, 0), (W.shift i, shiftMask)]]
     ++
 
@@ -101,6 +50,11 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
+    where
+    modm = modMask conf
+    smodm = modm .|. shiftMask
+    numsyms = [xK_exclam, xK_at, xK_numbersign, xK_dollar, xK_percent,
+        xK_asciicircum, xK_ampersand, xK_asterisk, xK_parenleft, xK_parenright]
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
@@ -108,15 +62,13 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster))
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
 
     -- mod-button2, Raise the window to the top of the stack
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
 
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster))
 
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
@@ -173,14 +125,12 @@ main = xmonad defaultConfig {
         borderWidth        = 3,
         modMask            = mod4Mask,
         numlockMask        = mod2Mask,
-        workspaces         = ["1","2","3","4","5","6","7","8","9"],
+        workspaces         = [[c] | c <- ['1'..'9']],
         normalBorderColor  = "#dddddd",
         focusedBorderColor = "#ff0000",
-
       -- key bindings
         keys               = myKeys,
         mouseBindings      = myMouseBindings,
-
       -- hooks, layouts
         layoutHook         = myLayout,
         manageHook         = myManageHook,
